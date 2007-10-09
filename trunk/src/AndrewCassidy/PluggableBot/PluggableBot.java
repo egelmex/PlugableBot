24ed5c7fbc05a02c1f0dbc59463fca973606e7d3
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2007-10-09 18:06:57 +0000

    basics are there now

--- trunk/src/AndrewCassidy/PluggableBot/PluggableBot.java
+++ trunk/src/AndrewCassidy/PluggableBot/PluggableBot.java
@@ -22,19 +22,121 @@ public class PluggableBot extends PircBot {
     private static String nick = "Bob";
     private static String server = "irc.freenode.net";
     private static PluggableBot b = new PluggableBot();
+    private static PluginLoader l = new PluginLoader();
+    private static ArrayList<String> channels = new ArrayList<String>();
     
     public static void main(String[] args)
     {
+        try
+        {
+            loadSettings();
+        }
+        catch (Exception e)
+        {
+            System.err.println("Failed to process config file: "+e.getMessage());
+            System.exit(0);
+        }
         
+        try
+        {
+            b.setName(nick);
+            b.connect(server);
+            for (String s : channels)
+                b.joinChannel(s);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Could not connect to server: "+e.getMessage());
+            System.exit(0);
+        }
     }
     
-    private static void loadSettings()
+    private static void loadSettings() throws Exception
     {
+        String context = "", line;
+        FileReader fr = new FileReader("config");
+        BufferedReader br = new BufferedReader(fr);
         
+        while ((line = br.readLine()) != null)
+        {
+            if (line.startsWith("[") && line.endsWith("]"))
+            {
+                context = line.substring(1, line.length()-1);
+            }
+            else
+            {
+                if (line.trim().length() == 0)
+                    continue;
+                else if (context.equals("nick"))
+                    nick = line;
+                else if (context.equals("server"))
+                    server = line;
+                else if (context.equals("channels"))
+                    channels.add(line);
+                else if (context.equals("plugins"))
+                    loadPlugin(line);
+            }
+        }
     }
     
-    private static void LoadPlugin(String name)    
+    private static void loadPlugin(String name)    
     {
-        
+        try
+        {
+            Plugin p = l.loadPlugin(name);
+            loadedPlugins.put(name, p);
+        }
+        catch (Exception ex)
+        {
+            System.err.println("Failed to load plugin: "+ex.getMessage());
+        }
+    }
+    
+    protected void onAction(String sender, String login, String hostname, String target, String action) 
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onAction(sender, login, hostname, target, action);
+    }
+    
+    protected void onJoin(String channel, String sender, String login, String hostname)
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onJoin(channel, sender, login, hostname);
+    }
+    
+    protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason)
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onKick(channel, kickerNick, kickerLogin, kickerHostname, recipientNick, reason);
+    }
+    protected void onMessage(String channel, String sender, String login, String hostname, String message)
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onMessage(channel, sender, login, hostname, message);    
+    }
+    protected void onPart(String channel, String sender, String login, String hostname)
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onPart(channel, sender, login, hostname);
+    }
+    protected void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason)
+    {
+        for (Plugin p : loadedPlugins.values())
+            p.onQuit(sourceNick, sourceLogin, sourceHostname, reason);
+    }
+    
+    public static String Nick()
+    {
+        return b.getNick();
+    }
+            
+    public static void Action(String target, String action) 
+    {
+        b.sendAction(target, action);
+    }
+    
+    public static void Message(String target, String action) 
+    {
+        b.sendMessage(target, action);
     }
 }
--- trunk/src/AndrewCassidy/PluggableBot/Plugin.java
+++ trunk/src/AndrewCassidy/PluggableBot/Plugin.java
@@ -14,5 +14,10 @@ package AndrewCassidy.PluggableBot;
  * @author AndyC
  */
 public interface Plugin {
-    
+    void onAction(String sender, String login, String hostname, String target, String action);
+    void onJoin(String channel, String sender, String login, String hostname);
+    void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason);
+    void onMessage(String channel, String sender, String login, String hostname, String message);
+    void onPart(String channel, String sender, String login, String hostname);
+    void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason);
 }
