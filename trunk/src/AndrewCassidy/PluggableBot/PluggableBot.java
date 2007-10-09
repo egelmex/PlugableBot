9ca3b425887f308ffe289db7f57fa6f4038cdec9
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2007-10-09 13:10:54 +0000

    main file structure

---
+++ trunk/src/AndrewCassidy/PluggableBot/PluggableBot.java
@@ -0,0 +1,30 @@
+/*
+ * PluggableBot.java
+ *
+ * Created on 09 October 2007, 14:04
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package AndrewCassidy.PluggableBot;
+
+import org.jibble.pircbot.*;
+/**
+ *
+ * @author AndyC
+ */
+public class PluggableBot extends PircBot {
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    /** Creates a new instance of PluggableBot */
+    public PluggableBot() {
+        
+    }
+    
+    
+}
---
+++ trunk/src/AndrewCassidy/PluggableBot/PluginInterface.java
@@ -0,0 +1,18 @@
+/*
+ * PluginInterface.java
+ *
+ * Created on 09 October 2007, 14:06
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package AndrewCassidy.PluggableBot;
+
+/**
+ *
+ * @author AndyC
+ */
+public interface PluginInterface {
+    
+}
