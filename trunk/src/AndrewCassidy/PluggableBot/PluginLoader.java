5aead817892bed2fc345d6a7cc544fc4c30ceabd
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2007-10-09 18:24:04 +0000

    added PluginLoader.java

---
+++ trunk/src/AndrewCassidy/PluggableBot/PluginLoader.java
@@ -0,0 +1,57 @@
+/*
+ * PluginLoader.java
+ *
+ * Created on 09 October 2007, 18:29
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package AndrewCassidy.PluggableBot;
+
+import java.io.*;
+/**
+ *
+ * @author Administrator
+ */
+public class PluginLoader extends ClassLoader {
+    
+    /** Creates a new instance of PluginLoader */
+    public PluginLoader() {
+        
+    }
+    
+    public Plugin loadPlugin(String name) throws Exception
+    {
+        Class c = null;
+        try
+        {
+            c = findClass(name);
+        }
+        catch (Exception e)
+        {
+            throw new InstantiationException("Class file could not be loaded");
+        }
+        return (Plugin)c.newInstance();
+    }
+    
+    protected Class findClass(String name) {
+        byte[] b = loadClassData(name);
+        return defineClass(name, b, 0, b.length);
+    }
+
+    private byte[] loadClassData(String name) {
+        try {
+            FileInputStream s = new FileInputStream("plugins/" + name + ".class");
+        
+            byte[] b = new byte[(int)s.getChannel().size()];
+            s.read(b);
+            return b;
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
