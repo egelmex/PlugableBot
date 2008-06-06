defc9bc402ed7a3e3fba9cbe9abcb5a8ab8195f6
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2008-06-06 09:27:25 +0000

    mailinfo

---
+++ trunk/src/Mailinfo.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import AndrewCassidy.PluggableBot.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/**
+ *
+ * @author AndyC
+ */
+public class Mailinfo implements Plugin {
+
+    private static final String COMMAND = "/usr/local/bin/mailinfo";
+    
+    public void onAction(String sender, String login, String hostname, String target, String action) {
+        
+    }
+
+    public void onJoin(String channel, String sender, String login, String hostname) {
+        
+    }
+
+    public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+        
+    }
+
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        if (message.startsWith("!mailinfo") && login.endsWith("@raptor.kent.ac.uk"))
+        {
+            try
+            {
+              Process exec = Runtime.getRuntime().exec(COMMAND + " " + login.replace("@raptor.kent.ac.uk", ""));
+              BufferedReader br = new BufferedReader(new InputStreamReader(exec.getInputStream()));
+              // headers
+              br.readLine();
+              // extra
+              PluggableBot.Message(channel, sender + ": " + br.readLine());
+              exec.waitFor();
+              br.close();
+            }
+            catch (Exception e)
+            {
+                
+            }
+        }
+    }
+
+    public void onPart(String channel, String sender, String login, String hostname) {
+        
+    }
+
+    public void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
+        
+    }
+
+    public void onPrivateMessage(String sender, String login, String hostname, String message) {
+        
+    }
+
+    public String getHelp() {
+        return "Type !mailinfo <login> to get email info for the specified login";
+    }
+
+}
