6a39815b8b79fe63323341294b3c56dfe6f7a280
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2008-10-14 18:30:19 +0000

    made a copy

---
+++ Dns/tags/0.1/src/Dns.java
@@ -0,0 +1,54 @@
+/*
+ * DNS.java
+ *
+ * Created on 19 October 2007, 17:59
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+import AndrewCassidy.PluggableBot.*;
+import java.net.InetAddress;
+/**
+ *
+ * @author Administrator
+ */
+public class Dns implements Plugin {
+    public void onAction(String sender, String login, String hostname, String target, String action) {
+    }
+
+    public void onJoin(String channel, String sender, String login, String hostname) {
+    }
+
+    public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+    }
+
+    public void onMessage(String channel, String sender, String login, String hostname, String message) {
+        if (message.toLowerCase().startsWith("!dns"))
+        {
+            try
+            {
+                PluggableBot.Message(channel, InetAddress.getByName(message.substring(5).trim()).getHostAddress());
+            }
+            catch(Exception e)
+            {
+                PluggableBot.Message(channel, "Sorry, I couldn't find that host");
+            }
+        }
+    }
+
+    public void onPart(String channel, String sender, String login, String hostname) {
+    }
+
+    public void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
+    }
+
+    public String getHelp() {
+        return "This plugin lets me resolve hostnames the their ip addresses. Type !dns <hostname> to use.";
+    }
+    public void onPrivateMessage(String sender, String login, String hostname, String message) {
+       
+    }
+    
+    public void unload() {}
+}
