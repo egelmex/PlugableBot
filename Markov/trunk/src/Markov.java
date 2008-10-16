5d6098c727af303299ca1c230557c5ae565a2e66
andee <andee@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2008-10-16 09:27:45 +0000

    changed format string

--- Markov/trunk/src/Markov.java
+++ Markov/trunk/src/Markov.java
@@ -33,7 +33,7 @@ public class Markov implements Plugin {
         {
             File f = new File("Markov2.db4o");
             double size = (double)f.length() / 1048576f;
-            PluggableBot.Message(channel, "My dictionary currently holds " + m.getWordCount() + " words and " + m.getConnectionCount() + " word associations. My dictionary file is " + String.format("%1$2f", size) + " MB");
+            PluggableBot.Message(channel, "My dictionary currently holds " + m.getWordCount() + " words and " + m.getConnectionCount() + " word associations. My dictionary file is " + String.format("%1$.2f", size) + " MB");
         }
         else
         {        
