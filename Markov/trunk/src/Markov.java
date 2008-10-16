dd6c350d69ff71f1e862e61d150043ef6bfca12f
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2008-10-16 13:52:36 +0000

    Ignore stuff

--- Markov/trunk/src/Markov.java
+++ Markov/trunk/src/Markov.java
@@ -1,3 +1,7 @@
+import java.io.File;
+
+import org.pluggableBot.IgnoreLib;
+
 import AndrewCassidy.PluggableBot.PluggableBot;
 import AndrewCassidy.PluggableBot.Plugin;
 import Markov2.MarkovString;
@@ -12,55 +16,77 @@ import java.io.*;
  */
 
 /**
- *
+ * 
  * @author Administrator
  */
 public class Markov implements Plugin {
-    
-    private MarkovString m = new MarkovString();
 
-    public void onAction(String sender, String login, String hostname, String target, String action) {
-    }
+	private MarkovString m = new MarkovString();
+	private IgnoreLib ignore = new IgnoreLib(this, "ignore");
+	private IgnoreLib ignoreLearn = new IgnoreLib(this, "learn");
 
-    public void onJoin(String channel, String sender, String login, String hostname) {
-    }
+	public void onAction(String sender, String login, String hostname,
+			String target, String action) {
+	}
 
-    public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
-    }
+	public void onJoin(String channel, String sender, String login,
+			String hostname) {
+	}
 
-    public void onMessage(String channel, String sender, String login, String hostname, String message) {
-        if (message.startsWith("!bobstats"))
-        {
-            File f = new File("Markov2.db4o");
-            double size = (double)f.length() / 1048576f;
+	public void onKick(String channel, String kickerNick, String kickerLogin,
+			String kickerHostname, String recipientNick, String reason) {
+	}
+
+	public void onMessage(String channel, String sender, String login,
+			String hostname, String message) {
+		if (!ignore.ignore(sender)) {
+			if (message.startsWith("!bobstats")) {
+				File f = new File("Markov2.db4o");
+				double size = (double) f.length() / 1048576f;
             int[] stats = m.getStats();
             PluggableBot.Message(channel, "My dictionary currently holds " + stats[0] + " words and " + stats[1] + " word associations. My dictionary file is " + String.format("%1$.2f", size) + " MB");
-        }
-        else
-        {        
-            m.Learn(message);
-            if (message.toLowerCase().indexOf(PluggableBot.Nick().toLowerCase()) > -1)
-            {
-                PluggableBot.Message(channel, m.Generate());
-            }
-        }
-    }
+			} else {
+				if (!ignoreLearn.ignore(sender)) {
+					m.Learn(message);
+				}
+				if (message.toLowerCase().indexOf(
+						PluggableBot.Nick().toLowerCase()) > -1) {
+					PluggableBot.Message(channel, m.Generate());
+				}
+			}
+		}
+	}
 
-    public void onPart(String channel, String sender, String login, String hostname) {
-    }
+	public void onPart(String channel, String sender, String login,
+			String hostname) {
+	}
 
-    public void onQuit(String sourceNick, String sourceLogin, String sourceHostname, String reason) {
-    }
+	public void onQuit(String sourceNick, String sourceLogin,
+			String sourceHostname, String reason) {
+	}
 
-    public String getHelp() {
-        return "The Markov plugin is a simple implementation of Markov chains. This plugin allows me to 'Learn' from what is said in the channel and be able to peice together sentences.";
-    }
-    public void onPrivateMessage(String sender, String login, String hostname, String message) {
-       
-    }
-    
-    public void unload()
-    {
-        m.cleanup();
-    }
+	public String getHelp() {
+		return "The Markov plugin is a simple implementation of Markov chains. This plugin allows me to 'Learn' from what is said in the channel and be able to peice together sentences.";
+	}
+
+	public void onPrivateMessage(String sender, String login, String hostname,
+			String message) {
+		if (!ignore.ignore(sender)) {
+			if (message.startsWith("IgnoreAll ")) {
+				ignore.addIgnore(message.substring("ignoreAll ".length()).trim());
+			} else if (message.startsWith("UnignoreAll ")) {
+				ignore.removeIgnore(message.substring("UnignoreAll ".length())
+						.trim());
+			} else if (message.startsWith("Ignore ")) {
+				ignoreLearn.addIgnore(message.substring("ignore ".length()).trim());
+			} else if (message.startsWith("Unignore ")) {
+				ignoreLearn.removeIgnore(message.substring("unignore ".length())
+						.trim());
+			}
+		}
+	}
+
+	public void unload() {
+		m.cleanup();
+	}
 }
---
+++ ignoreLib/src/org/pluggableBot/IgnoreLib.java
@@ -0,0 +1,102 @@
+package org.pluggableBot;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.channels.IllegalSelectorException;
+import java.util.ArrayList;
+
+public class IgnoreLib {
+	
+	String ingoreListName;
+	File ignoreListf;
+	ArrayList<String> ignoreLista = new ArrayList<String>();
+
+	public IgnoreLib(Object object, String name) {
+		this.ingoreListName = object.getClass().getName() + "-" + name;
+		openIgnoreListf();
+	}
+	
+	public IgnoreLib(Class clas, String name) {
+		this.ingoreListName = clas.getName() + "-" + name;
+		openIgnoreListf();
+	}
+	
+	private void openIgnoreListf() {
+		FileReader fr = null;
+		BufferedReader br = null;
+        try {
+            File f = new File(ingoreListName);
+            if (!f.exists()) {
+                System.out.println("Creating ignore list : " + ingoreListName);
+                f.createNewFile();
+            }
+            
+                fr = new FileReader(f);
+                br = new BufferedReader(fr);
+                
+                String ignore;
+                
+                while ((ignore = br.readLine()) != null) {
+                    ignoreLista.add(ignore);
+                }
+            } catch (FileNotFoundException ex) {
+                throw new IllegalSelectorException();
+            } catch (IOException ex) {
+                throw new IllegalSelectorException();
+            } finally {
+            	if (br != null)
+					try { br.close(); } catch (IOException e) {}
+            	if (fr != null)
+            		try { fr.close(); } catch (IOException e) {}
+            }
+	}
+	
+	public boolean ignore(String name) {
+		return ignoreLista.contains(name.toLowerCase());
+	}
+	
+	public void addIgnore (String name) {
+		if (!ignoreLista.contains(name.toLowerCase())) {
+			ignoreLista.add(name.toLowerCase());
+			serialiseIgnoreList(ignoreLista);
+		}
+	}
+	
+	public void removeIgnore(String name) {
+		if (!ignoreLista.contains(name.toLowerCase())) {
+			ignoreLista.remove(name.toLowerCase());
+			ignoreLista.add(name.toLowerCase());
+			serialiseIgnoreList(ignoreLista);
+		}
+	}
+	
+	private void serialiseIgnoreList(ArrayList<String> list) {
+		BufferedWriter out = null;
+		FileWriter fw = null;
+		
+	    try {
+	    	fw = new FileWriter(ignoreListf);
+	        out = new BufferedWriter(fw);
+	        
+	        for (String s : list) {
+	        	out.write(s);
+	        }
+	        
+	    } catch (IOException e) {
+	    	System.out.println("Failing to serialse ignore list :(");
+	    } finally {
+        	if (fw != null)
+				try { fw.close(); } catch (IOException e) {}
+        	if (out != null)
+        		try { out.close(); } catch (IOException e) {}
+	    }
+
+	}
+	
+	
+}
