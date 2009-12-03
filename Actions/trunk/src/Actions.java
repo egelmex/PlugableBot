b17f9f19aff19569eb28aa4ad458f8fa06ac0da1
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2009-12-03 13:01:38 +0000

    added loads of options to this plugin. can now list actions, will add deleting actions from irc at some point. can also reaload the action file.

--- Actions/trunk/src/Actions.java
+++ Actions/trunk/src/Actions.java
@@ -18,31 +18,24 @@ public class Actions implements Plugin {
     
     private ArrayList<String> attacks = new ArrayList<String>();
     private Random r = new Random();
+    private boolean adminEnabled = false;
+    
     
     public Actions()
     {
-        try
-        {
-            FileReader fr = new FileReader("Action");
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            while ((line = br.readLine()) != null)
-            {
-                attacks.add(line);
-            }
-        }
-        catch (Exception e)
-        {
-            throw new IllegalStateException("Unable to load actions file.");
-        }
+    	reloadActions();
                 
     }
     
     public void onAction(String sender, String login, String hostname, String target, String action) {
         if (action.toLowerCase().indexOf(PluggableBot.Nick().toLowerCase()) > -1)
         {
-            String a = attacks.get(r.nextInt(attacks.size()));
-            PluggableBot.Action(target, a.replaceAll("%SENDER", sender).replaceAll("%NAME", sender));
+        	if (attacks.size() > 0) {
+	            String a = attacks.get(r.nextInt(attacks.size()));
+	            PluggableBot.Action(target, a.replaceAll("%SENDER", sender).replaceAll("%NAME", sender));
+        	} else {
+        		PluggableBot.Action(target, "I don't know what to do about that :(".replaceAll("%SENDER", sender).replaceAll("%NAME", sender));
+        	}
         }
     }
 
@@ -51,35 +44,61 @@ public class Actions implements Plugin {
 
     public void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
     }
+    public void reloadActions() {
+    	attacks = new ArrayList<String>();
+		try
+		{
+		    FileReader fr = new FileReader("Action");
+		    BufferedReader br = new BufferedReader(fr);
+		    String line;
+		    while ((line = br.readLine()) != null)
+		    {
+		        attacks.add(line);
+		    }
+		}
+		catch (Exception e)
+		{
+		    throw new IllegalStateException("Unable to load actions file.");
+		}
+    }
+    
+    public boolean saveActions() {
+    	boolean ok = true;
+        try
+        {
+            FileWriter fw = new FileWriter("Action");
+            BufferedWriter bw = new BufferedWriter(fw);
+            for(String si : attacks){
+            	bw.write(si + "\n");
+            }
+            bw.flush();
+            bw.close();
+            fw.close();
+        }
+        catch (Exception e)
+        {
+            ok=false;
+        }
+        return ok;
+    }
 
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
-        if (message.startsWith("!addaction"))
+        if (message.startsWith("!addaction")  && adminEnabled)
         {
             String s = message.substring(11);
             attacks.add(s);
-            boolean ok = true;
-            
-            try
-            {
-                FileWriter fw = new FileWriter("Action");
-                BufferedWriter bw = new BufferedWriter(fw);
-                for(String si : attacks){
-                	bw.write(si + "\n");
-                }
-                bw.flush();
-                bw.close();
-                fw.close();
-            }
-            catch (Exception e)
-            {
-                ok=false;
-            }
+            boolean ok = saveActions();
             if (ok) {
             	PluggableBot.Message(channel, sender + ": I will remeber that!");
             } else {
             	PluggableBot.Message(channel, sender + ": Whoops I am being forgetful today!" );
             	attacks.remove(s);
             }
+        } else if (message.toLowerCase().startsWith("!listactions") && adminEnabled) {
+        	PluggableBot.Message(sender, "My retorts are:");
+        	for (String s : attacks) {
+        		PluggableBot.Message(sender, s);
+        	}
         }
     }
 
@@ -91,7 +110,12 @@ public class Actions implements Plugin {
     
     public String getHelp()
     {
-        return "This plugin makes me retaliate to any action performed against me. Use !addaction followed by the action, using %NAME and %SENDER as placeholders to add a new one.";
+    	if (adminEnabled) {
+    		return "This plugin makes me retaliate to any action performed against me. Use !addaction followed by the action, using %NAME and %SENDER as placeholders to add a new one. !listactions";
+    	} else {
+    		return "This plugin makes me retaliate to any action performed against me.";
+    	}
+        
     }
 
     public void onPrivateMessage(String sender, String login, String hostname, String message) {
@@ -103,6 +127,14 @@ public class Actions implements Plugin {
 	@Override
 	public void onAdminMessage(String sender, String login, String hostname,
 			String message) {
+		if (message.toLowerCase().startsWith("!actionenableadmin")) {
+			this.adminEnabled = true;
+		} else if (message.toLowerCase().startsWith("!actiondisableadmin")){
+			this.adminEnabled = true;
+		} else if (message.toLowerCase().startsWith("!actionreload")){
+			reloadActions();
+		}
+		
 		
 	}
 }
