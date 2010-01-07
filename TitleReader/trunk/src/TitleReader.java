c8f1ba03c1351e6423c5cf49490cd7ae3f2d1377
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-01-07 14:31:04 +0000

    fixed urls broken by :

--- TitleReader/trunk/src/TitleReader.java
+++ TitleReader/trunk/src/TitleReader.java
@@ -4,8 +4,19 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 
 import AndrewCassidy.PluggableBot.PluggableBot;
 import AndrewCassidy.PluggableBot.Plugin;
@@ -70,10 +81,53 @@ public class TitleReader implements Plugin {
 						while ((inputLine = in.readLine()) != null)
 							sb.append(inputLine);
 
+						DefaultHttpClient httpclient = new DefaultHttpClient();
+						HttpPost post = new HttpPost("http://snipr.com/site/getsnip");
+					    
+						
+						/*
+						 * $postfield =  'sniplink='  . $sniplink  . '&' .
+              'snipnick='  . $snipnick  . '&' .
+              'snipuser='  . $snipuser  . '&' .
+              'snipapi='   . $snipapi   . '&' .
+              'sniptitle=' . $sniptitle . '&' .
+              'snipowner=' . $snipowner . '&' .
+              'snipformat='. $snipformat. '&' .
+              'snippk='    . $snippk   
+						 * 
+						 * 
+						 */
+							List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+				        nvps.add(new BasicNameValuePair("sniplink", url.toString()));
+				        nvps.add(new BasicNameValuePair("snipuser", "mex"));
+				        nvps.add(new BasicNameValuePair("snipapi", "b23ff81382e8ff92307f4c56951f2815"));
+				        nvps.add(new BasicNameValuePair("snipnick", ""));
+				        nvps.add(new BasicNameValuePair("sniptitle", ""));
+				        nvps.add(new BasicNameValuePair("snipowner", ""));
+				        nvps.add(new BasicNameValuePair("snipformat", "simple"));
+				        nvps.add(new BasicNameValuePair("snippk", ""));
+				        
+				        post.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+						
+				        HttpResponse response = httpclient.execute(post);
+				        HttpEntity entity = response.getEntity();
+				        
+				        
+						BufferedReader in2 = new BufferedReader(
+								new InputStreamReader(entity.getContent()));
+
+						String inputLine2;
+						StringBuilder sb2 = new StringBuilder();
+
+						while ((inputLine2 = in2.readLine()) != null)
+							sb2.append(inputLine2);
+				        
+				        System.out.println(sb2);
+						
 						Matcher m = p.matcher(sb);
 						if (m.find()) {
-							PluggableBot.Message(channel, url.toString() + ": " +  m.group(1));
-							System.out.println(url.toString() + ": " +  m.group(1));
+							PluggableBot.Message(channel, sb2 + " : " +  m.group(1));
+							System.out.println(sb2 + " : " +  m.group(1));
 						}
 					}
 
