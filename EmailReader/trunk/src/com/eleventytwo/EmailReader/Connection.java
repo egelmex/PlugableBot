646287ca77eb3d2f467dc4f90db2e7ba030bca2a
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-01-13 23:03:54 +0000

    fixed :)

--- EmailReader/trunk/src/com/eleventytwo/EmailReader/Connection.java
+++ EmailReader/trunk/src/com/eleventytwo/EmailReader/Connection.java
@@ -1,21 +1,17 @@
 package com.eleventytwo.EmailReader;
 
-import java.security.Security;
 import java.util.Properties;
 
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
-import javax.mail.Session;
-import javax.mail.Store;
-import com.sun.net.ssl.internal.ssl.Provider;
 
 import AndrewCassidy.PluggableBot.PluggableBot;
 
 public class Connection implements Runnable {
 
-	Properties props;
+	java.util.Properties props;
 
 	private static final int MAX_RETRIES = 3;
 
@@ -27,40 +23,36 @@ public class Connection implements Runnable {
 	@Override
 	public void run() {
 		int retries = 0;
-		Store store = null;
+		javax.mail.Store store = null;
 		while (retries < MAX_RETRIES) {
 			System.out.println("retries = " + retries);
 			if (store == null || !store.isConnected()) {
 				boolean ok = false;
 
-				String port = props.getProperty("port");
-				String username = props.getProperty("username").trim();
-				String password = props.getProperty("password").trim();
-				String server = props.getProperty("server").trim();
 				try {
-			
-					Security
-							.addProvider(new Provider());
-					Properties ssl_props = new java.util.Properties();
-					ssl_props.setProperty("mail.imap.socketFactory.class",
-							"javax.net.ssl.SSLSocketFactory");
-					ssl_props.setProperty("mail.imap.socketFactory.fallback",
-							"false");
-					ssl_props.setProperty("mail.imap.socketFactory.port", port);
+					String port = props.getProperty("port");
+					String username = props.getProperty("username").trim();
+					String password = props.getProperty("password").trim();
+					String server = props.getProperty("server").trim();
+					
+						java.security.Security
+								.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+						java.util.Properties props = new java.util.Properties();
+						props.setProperty("mail.imap.socketFactory.class",
+								"javax.net.ssl.SSLSocketFactory");
+						props.setProperty("mail.imap.socketFactory.fallback", "false");
+						props.setProperty("mail.imap.socketFactory.port", port);
 
-					Session session = javax.mail.Session.getInstance(ssl_props);
-
-					String imap_url = "imap://" + username + ":" + password
-							+ "@" + server + "/";
-					System.out.println(imap_url);
-					store = session.getStore(new javax.mail.URLName(imap_url));
-					store.connect();
-					ok = true;
-
-				} catch (NoSuchProviderException e) {
-					e.printStackTrace();
-				} catch (MessagingException e) {
-					e.printStackTrace();
+						javax.mail.Session session = javax.mail.Session.getInstance(props);
+						store = session.getStore(new javax.mail.URLName(
+								"imap://" + username + ":" + password + "@" + server + "/"));
+						store.connect();
+						System.out.println("connected to store.");
+						ok = true;
+				} catch (NoSuchProviderException e1) {
+					e1.printStackTrace();
+				} catch (MessagingException e1) {
+					e1.printStackTrace();
 				}
 
 				if (ok) {
@@ -84,11 +76,11 @@ public class Connection implements Runnable {
 
 	}
 
-	public Properties getProps() {
+	public java.util.Properties getProps() {
 		return props;
 	}
 
-	private void read(Store store, String mailbox) {
+	private void read(javax.mail.Store store, String mailbox) {
 		System.out.println("reading mailbox");
 		try {
 			Folder folder = store.getFolder(mailbox);
