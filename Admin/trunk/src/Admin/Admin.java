e78505e70568ae06a0c1e251a6884c50bf6cea6c
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-04-19 21:27:00 +0000

    lots of commits coming

--- Admin/trunk/src/Admin/Admin.java
+++ Admin/trunk/src/Admin/Admin.java
@@ -1,5 +1,5 @@
+package Admin;
 import AndrewCassidy.PluggableBot.DefaultPlugin;
-import AndrewCassidy.PluggableBot.PluggableBot;
 
 public class Admin extends DefaultPlugin {
 
@@ -13,17 +13,17 @@ public class Admin extends DefaultPlugin {
 			String message) {
 		if (message.startsWith("unload")) {
 			bot.unloadPlugin(message.substring(7));
-			PluggableBot.Message(sender, "unloaded");
+			bot.Message(sender, "unloaded");
 		} else if (message.startsWith("reload")) {
 			bot.unloadPlugin(message.substring(7));
 			bot.loadPlugin(message.substring(7));
-			PluggableBot.Message(sender, "reloaded");
+			bot.Message(sender, "reloaded");
 		} else if (message.startsWith("join")) {
 			bot.joinChannel(message.substring(5));
-			PluggableBot.Message(sender, "joined");
+			bot.Message(sender, "joined");
 		} else if (message.startsWith("part")) {
 			bot.partChannel(message.substring(5));
-			PluggableBot.Message(sender, "left");
+			bot.Message(sender, "left");
 		} else if (message.startsWith("load")) {
 			bot.loadPlugin(message.substring(5));
 		}
