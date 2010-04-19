bc8a7dbbcaa3365adf43e9a26dad72a9682d53de
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-04-19 21:43:15 +0000

    lots of commits coming

--- Pounce/trunk/src/Pounce/Pounce.java
+++ Pounce/trunk/src/Pounce/Pounce.java
@@ -1,3 +1,4 @@
+package Pounce;
 /*
  * Pounce.java
  *
@@ -14,13 +15,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import AndrewCassidy.PluggableBot.DefaultPlugin;
-import AndrewCassidy.PluggableBot.PluggableBot;
 
 /**
  * 
  * @author AndyC & Mex
  */
-public class Pounce extends DefaultPlugin{
+public class Pounce extends DefaultPlugin {
 
 	private ArrayList<String> pounces = new ArrayList<String>();
 	private Random r = new Random();
@@ -39,68 +39,66 @@ public class Pounce extends DefaultPlugin{
 		}
 	}
 
-
 	public void onJoin(String channel, String sender, String login,
 			String hostname) {
-		if (sender.equals(PluggableBot.Nick())) return;
-		
+		if (sender.equals(bot.Nick()))
+			return;
+
 		if (r.nextInt(probability) == 1) {
-			PluggableBot
-					.Message(channel, pounces.get(r.nextInt(pounces.size()))
-							.replaceAll("%NAME", sender));
+			bot.Message(channel, pounces.get(r.nextInt(pounces.size()))
+					.replaceAll("%NAME", sender));
 		}
 	}
 
 	public void onKick(String channel, String kickerNick, String kickerLogin,
 			String kickerHostname, String recipientNick, String reason) {
-		PluggableBot.Message(channel, "haha " + recipientNick
-				+ " was a dick anyway.");
+		bot.Message(channel, "haha " + recipientNick + " was a dick anyway.");
 	}
 
 	public void onMessage(String channel, String sender, String login,
 			String hostname, String message) {
-		
+
 		String tMessage = message.trim();
 		if (message.startsWith("!addpounce")) {
 
 			String s = message.substring(11);
 			if (pounces.contains(s)) {
-				PluggableBot.Message(channel, sender
+				bot.Message(channel, sender
 						+ ": Stop feeding me the same junk!");
 			} else {
 				pounces.add(s);
 				boolean ok = saveActions();
 				if (ok) {
-					PluggableBot.Message(channel, sender
-							+ ": I will remeber that!");
+					bot.Message(channel, sender + ": I will remeber that!");
 				} else {
-					PluggableBot.Message(channel, sender
+					bot.Message(channel, sender
 							+ ": Whoops I am being forgetful today!");
 					pounces.remove(s);
 				}
 			}
-		} else if (message.startsWith("ls ") || message.startsWith("cd ")|| message.startsWith("cp ") ||
-				   message.equals("ls")      || message.equals("cd")) {
-			PluggableBot.Message(channel, sender + ": Wrong window?");
+		} else if (message.startsWith("ls ") || message.startsWith("cd ")
+				|| message.startsWith("cp ") || message.equals("ls")
+				|| message.equals("cd")) {
+			bot.Message(channel, sender + ": Wrong window?");
 		} else if (message.trim().toLowerCase().equals("ping")) {
-			PluggableBot.Message(channel, sender +": PONG!");
+			bot.Message(channel, sender + ": PONG!");
 		} else if (message.trim().toLowerCase().startsWith("win ")) {
-			PluggableBot.Message(channel, sender +": Fail!");
+			bot.Message(channel, sender + ": Fail!");
 		} else if (message.startsWith("rm ") || tMessage.equals("rm")) {
-			PluggableBot.Message(channel, sender + ": delete porn [y/n]");
+			bot.Message(channel, sender + ": delete porn [y/n]");
 		} else if (tMessage.equals("ps")) {
-			PluggableBot.Message(channel, "PID      TTY     TIME        CMD");
-			PluggableBot.Message(channel, "18101    pts/5   00:00:00    bash");
-			PluggableBot.Message(channel, "18187    pts/5   00:00:00    'firefox.com hotbabes.com'");
-			PluggableBot.Message(channel, "18200    pts/5   00:00:00    ps");
-			
-			
-		} else if (message.toLowerCase().startsWith("identify ")) {
-			PluggableBot.Message(channel, "Hey everyone! "+ sender +"'s password is " + message.substring(9).trim());
-			
-		} 
-	}
+			bot.Message(channel, "PID      TTY     TIME        CMD");
+			bot.Message(channel, "18101    pts/5   00:00:00    bash");
+			bot.Message(channel,
+					"18187    pts/5   00:00:00    'firefox.com hotbabes.com'");
+			bot.Message(channel, "18200    pts/5   00:00:00    ps");
 
+		} else if (message.toLowerCase().startsWith("identify ")) {
+			bot.Message(channel, "Hey everyone! " + sender + "'s password is "
+					+ message.substring(9).trim());
+
+		}
+	}
 
 	private boolean saveActions() {
 		boolean ok = true;
@@ -125,7 +123,6 @@ public class Pounce extends DefaultPlugin{
 				+ ". to add a new pounce, use !addpounce followed by the message, using %NAME as a placeholder.";
 	}
 
-
 	@Override
 	public void onAdminMessage(String sender, String login, String hostname,
 			String message) {
@@ -134,15 +131,13 @@ public class Pounce extends DefaultPlugin{
 			String s = message.substring(15);
 			try {
 				probability = Integer.parseInt(s);
-				PluggableBot.Message(sender, "prob set to " + probability);
+				bot.Message(sender, "prob set to " + probability);
 
 			} catch (Exception e) {
 			}
 		} else if (message.toLowerCase().trim().equals("!help Actions")) {
-			
-			PluggableBot
-			.Message(sender,
-					"Actions Admin Help: !pounce setProb <prob>");
+
+			bot.Message(sender, "Actions Admin Help: !pounce setProb <prob>");
 		}
 
 	}
