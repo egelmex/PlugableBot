e660169db63987be3e868154c93e3df359dab750
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-07-12 17:45:54 +0000

    some work on the db visulisation

--- Markov/trunk/src/Markov/MarkovExplorer.java
+++ Markov/trunk/src/Markov/MarkovExplorer.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import net.sf.ooweb.http.RequestState;
 import net.sf.ooweb.objectmapping.Controller;
+import net.sf.ooweb.objectmapping.Exclude;
 
 import com.db4o.ObjectSet;
 
@@ -11,7 +12,7 @@ import com.db4o.ObjectSet;
 public class MarkovExplorer {
 
 	MarkovDatabase db;
-	
+
 	private static final Logger log = Logger.getLogger(Markov.class.getName());
 
 	public MarkovExplorer(MarkovDatabase db) {
@@ -19,62 +20,49 @@ public class MarkovExplorer {
 	}
 
 	public String index() {
-		String output = "";
-		try {
-			log.info("index: 1");
-			ObjectSet<MarkovLink> links = db.getLinks("[");
-			if (links == null) {
-				log.info("links was null :(");
-			} else {
-			log.info("index: 2");
-			for (MarkovLink link : links) {
-				if (link.getTo().getWord().equals("]")) {
-					
-				} else {
-					output += "<a href=\"/get?word=" +  link.getTo().getWord()+"\">" +  link.getTo().getWord() + "</a><br />\n";
-				}
-			}
-			log.info("index: 3");
-			
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return output;
-		// //List<MarkovNode> nodes = cache.get("[").getChildren();
-		// for (MarkovNode node : nodes) {
-		// output += node.getWord() + "\n";
-		// }
-		//    	
+		return getNode("[");
 
 	}
 
-	public String get(RequestState state) {
+	@Exclude
+	public String getNode(String id) {
+		log.info("Getting children of " + id);
 		String output = "";
+		output += "{id:\"";
+		output += id.equals("[")?"root":id;
+		output += "\",name:\"root\",data:{},children:[";
+
 		try {
-			log.info("index: 1");
-			ObjectSet<MarkovLink> links = db.getLinks((String)(state.getRequestArgs().get("word")));
-			output += "<h1>" + state.getRequestArgs().get("word") + "</h1>";
+			ObjectSet<MarkovLink> links = db.getLinks(id);
 			if (links == null) {
-				log.info("links was null :(");
+
 			} else {
-			log.info("index: 2");
-			for (MarkovLink link : links) {
-				if (link.getTo().getWord().equals("]")) {
-					
-				} else {
-					output += "<a href=\"/get?word=" +  link.getTo().getWord()+"\">" +  link.getTo().getWord() + "</a><br />\n";
+				for (MarkovLink link : links) {
+					if (link.getTo().getWord().equals("]")) {
+
+					} else {
+						String word = link.getTo().getWord();
+						output += "{id:\"" + word
+								+ "\", name:\"" + word
+								+ "\", data:{}, children:[]}";
+						if (links.hasNext()) {
+							output += ",";
+						}
+					}
 				}
+
 			}
-			log.info("index: 3");
-			
-			}
+			output += "]}";
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return output;
+	}
+	
+	public String get(RequestState state) {
+		
+		return getNode((String) state.getRequestArgs().get("word"));
 	}
 
 }
