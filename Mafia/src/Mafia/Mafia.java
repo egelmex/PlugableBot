fa8110f56df12bde7b717ce92d65aa833ab679e6
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-10-05 21:29:39 +0000

    more wibbeling for mafia

--- Mafia/src/Mafia/Mafia.java
+++ Mafia/src/Mafia/Mafia.java
@@ -1,16 +1,14 @@
 package Mafia;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import org.hibernate.hql.ast.tree.Case2Node;
 import org.jibble.pircbot.User;
-
-import jmathlib.toolbox.jmathlib.system.foreach;
 
 import com.PluggableBot.plugin.DefaultPlugin;
 
@@ -23,10 +21,11 @@ public class Mafia extends DefaultPlugin {
 	private static final String COMMAND_START = "!mafiastart";
 	private static final String COMMAND_JOIN = "!mafiajoin";
 	private static final String COMMAND_HIT = "!mafiahit";
-	private static final String COMMAND_KILL = "!mafiakill";
+	private static final String COMMAND_KILL = "!mafialockup";
 
 	private static final int TIMEOUT_JOIN = 10 * 1000;
 	private static final int TIMEOUT_HIT = 10 * 1000;
+	private static final int TIMEOUT_JAIL = 20 * 1000;
 	private static final int TIMEOUT_INVITE = 10 * 1000;
 
 	private Stage stage = Stage.loading;
@@ -42,114 +41,128 @@ public class Mafia extends DefaultPlugin {
 
 	@Override
 	public void load() {
+
 		bot.addCommand(COMMAND_START, this);
 		bot.addCommand(COMMAND_JOIN, this);
-		joinMafiaChannel();
-		joinGameChannel();
+
+		mafiaChannel = joinChannel("#mafiaboss");
+		bot.setTopic(mafiaChannel, "Mob Bosses hideout");
+		gameChannel = joinChannel("#mafia");
+		bot.setTopic(gameChannel, "Little Italy");
 		stage = Stage.noGame;
 
 	}
 
-	private void joinMafiaChannel() {
-		mafiaChannel = "#mafiaboss";
+	private String joinChannel(String prefix) {
+		String chan = prefix;
 		Random rng = new Random();
 		int number = rng.nextInt(999999);
-		mafiaChannel += number;
-
-		bot.joinChannel(mafiaChannel);
-
-		User[] users = bot.getUsers(mafiaChannel);
-		if (users.length == 1 && users[0].getNick() == bot.getNick()
-				&& users[0].isOp()) {
-			bot.setTopic(mafiaChannel, "Mob Bosses hideout");
-			bot.setMode(mafiaChannel, "ism");
+		chan += number;
+		bot.joinChannel(chan);
+		User[] users = bot.getUsers(chan);
+		if (users.length == 1 && users[0].getNick() == bot.getNick() && users[0].isOp()) {
+			bot.setMode(chan, "ism");
+			return chan;
 		} else {
-			bot.partChannel(mafiaChannel);
-			joinMafiaChannel();
+			bot.partChannel(chan);
+			return joinChannel(prefix);
 		}
-
 	}
 
-	private void joinGameChannel() {
-		mafiaChannel = "#mafia";
-		Random rng = new Random();
-		int number = rng.nextInt(999999);
-		mafiaChannel += number;
+	private void doStart(String channel, String sender) {
 
-		bot.joinChannel(mafiaChannel);
-
-		User[] users = bot.getUsers(mafiaChannel);
-		if (users.length == 1 && users[0].getNick() == bot.getNick()
-				&& users[0].isOp()) {
-			bot.setTopic(mafiaChannel, "Down town Italy");
-			bot.setMode(mafiaChannel, "ism");
-		} else {
-			bot.partChannel(gameChannel);
-			joinGameChannel();
+		switch (stage) {
+		case loading:
+			break;
+		case noGame:
+			this.gameChannel = channel;
+			players = new ArrayList<String>();
+			players.add(sender);
+			bot.sendMessage(channel, sender + " started a game of Mafia!");
+			bot.sendMessage(channel, "Joing the game with " + COMMAND_JOIN);
+			timer.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					chooseMafia();
+				}
+			}, TIMEOUT_JOIN);
+			break;
+		default:
+			bot.sendMessage(channel, sender + ": Sorry a game is already in progress.");
 		}
+	}
 
+	private void doJoin(String channel, String sender) {
+		switch (stage) {
+		case noGame:
+			bot.sendMessage(channel, sender + ": There is no current game, you can start one with "
+					+ COMMAND_START);
+			break;
+		case joining:
+			players.add(sender);
+			bot.sendInvite(sender, gameChannel);
+			break;
+		default:
+			bot.sendAction(channel, sender + ": You can not join a running game.");
+		}
+	}
+
+	private void doHit(String message, String sender, String channel) {
+		switch (stage) {
+		case mafiahit:
+			if (players.contains(message.trim())) {
+				hitMap.put(sender, message);
+				bot.sendMessage(channel, sender + ": OK.");
+				if (hitMap.size() == mafia.size()) {
+					timer.cancel();
+					timer = new Timer();
+					calculateHit();
+
+				}
+			} else {
+				bot.sendMessage(channel, sender + ": Unknown target");
+			}
+			break;
+		default: 
+			bot.sendMessage(channel, sender
+					+ ": You can only plan your hits under the cover of darkness.");
+			break;
+		}
+	}
+
+	private void doKill(String message, String sender, String channel) {
+		switch (stage) {
+		case slickermob:
+			if (players.contains(message.trim())) {
+				hitMap.put(sender, message);
+				bot.sendMessage(channel, sender + ": OK.");
+				if (hitMap.size() == players.size()) {
+					timer.cancel();
+					timer = new Timer();
+					calculateJail();
+				}
+			} else {
+				bot.sendMessage(channel, sender + ": Unknown target");
+			}
+			break;
+		default:
+			bot.sendMessage(channel, sender
+					+ ": You can only plan your hits under the cover of darkness.");
+			break;
+		}
 	}
 
 	@Override
-	public void onCommand(String command, String channel, String sender,
-			String login, String hostname, String message) {
+	public void onCommand(String command, String channel, String sender, String login,
+			String hostname, String message) {
 		if (command.equals(COMMAND_START)) {
-
-			switch (stage) {
-			case loading:
-				break;
-			case noGame:
-				this.gameChannel = channel;
-				players = new ArrayList<String>();
-				players.add(sender);
-				bot.sendMessage(channel, sender + " started a game of Mafia!");
-				bot.sendMessage(channel, "Joing the game with " + COMMAND_JOIN);
-				timer.schedule(new TimerTask() {
-					@Override
-					public void run() {
-						chooseMafia();
-					}
-				}, TIMEOUT_JOIN);
-				break;
-			default:
-				bot.sendMessage(channel, sender
-						+ ": Sorry a game is already in progress.");
-			}
-
+			doStart(channel, sender);
 		} else if (command.equals(COMMAND_JOIN)) {
-			switch (stage) {
-			case noGame:
-				bot.sendMessage(channel, sender
-						+ ": There is no current game, you can start one with "
-						+ COMMAND_START);
-				break;
-			case joining:
-				players.add(sender);
-				bot.sendInvite(sender, gameChannel);
-				break;
-			default:
-				bot.sendAction(channel, sender
-						+ ": You can not join a running game.");
-			}
-
+			doJoin(channel, sender);
 		} else if (command.equals(COMMAND_HIT) && channel.equals(mafiaChannel)) {
-			switch (stage) {
-			case mafiahit:
-				if (players.contains(message.trim())) {
-					hitMap.put(sender, message);
-					bot.sendMessage(channel, sender + ": OK.");
-				} else {
-					bot.sendMessage(channel, sender + ": Unknown target");
-				}
-				break;
-			default:
-				bot
-						.sendMessage(
-								channel,
-								sender
-										+ ": You can only plan your hits under the cover of darkness.");
-				break;
-			}
+			doHit(message, sender, channel);
+		} else if (command.equals(COMMAND_KILL) && channel.equals(gameChannel)) {
+			doKill(message, sender, channel);
 		}
 	}
 
@@ -166,20 +179,19 @@ public class Mafia extends DefaultPlugin {
 
 		}
 
-		bot.sendMessage(gameChannel,
-				"The mafia is loose, people are going to die");
+		bot.sendMessage(gameChannel, "The mafia is loose, people are going to die");
 
 		timer.schedule(new TimerTask() {
-			
+
 			@Override
 			public void run() {
 				checkMafia();
-				
+
 			}
 		}, TIMEOUT_INVITE);
 
 	}
-	
+
 	public void checkMafia() {
 		User[] mafiaU = bot.getUsers(mafiaChannel);
 		if (mafiaU.length < 2) {
@@ -196,26 +208,145 @@ public class Mafia extends DefaultPlugin {
 				users += player + " ";
 			}
 			bot.sendMessage(mafiaChannel, "The following people are still alive:" + users);
-			bot.sendMessage(mafiaChannel, "Choose who to kill with "+ COMMAND_HIT +" <name>");
+			bot.sendMessage(mafiaChannel, "Choose who to kill with " + COMMAND_HIT + " <name>");
 			bot.sendMessage(mafiaChannel, "You have " + TIMEOUT_HIT / 1000 + " seconds to choose");
 			timer.schedule(new TimerTask() {
-				
+
 				@Override
 				public void run() {
+					calculateHit();
 
-					
 				}
 			}, TIMEOUT_HIT);
 		}
 	}
-	
+
 	private void calculateHit() {
-		
+		bot.sendMessage(mafiaChannel, "Times up mobsters!");
+		stage = Stage.slickermob;
+
+		ArrayList<String> uniqueElements = new ArrayList<String>();
+		ArrayList<Integer> counts = new ArrayList<Integer>();
+
+		// traverse the array, collect the elements/counts
+		for (String element : hitMap.values()) {
+			int index = uniqueElements.indexOf(element);
+			// element has been seen:
+			if (index != -1) {
+				// increment its count
+				int newCount = counts.get(index) + 1;
+				counts.set(index, newCount);
+			} else {
+				// add it to uniqueElements, set its count to 1
+				uniqueElements.add(element);
+				counts.add(1);
+			}
+		}
+
+		// find maximum count, get the corresponding element
+		int maxCount = 0;
+		int index = -1;
+
+		for (int i = 0; i < counts.size(); i++) {
+			if (maxCount < counts.get(i)) {
+				maxCount = counts.get(i);
+				index = i;
+			}
+		}
+		String target = uniqueElements.get(index);
+
+		hitMap = new HashMap<String, String>();
+
+		bot.sendMessage(gameChannel, "The Mob has had enough of " + target);
+		bot.deVoice(gameChannel, target);
+		bot.deVoice(mafiaChannel, target);
+
+		bot.sendMessage(gameChannel, "Try and work out who the mafia are!");
+		String users = "";
+		for (String player : players) {
+			users += player + " ";
+		}
+		bot.sendMessage(mafiaChannel, "The following people are still alive:" + users);
+		bot.sendMessage(gameChannel, "Once you have made up your choice use " + COMMAND_KILL
+				+ " <target> to lock up a mobster");
+		bot.sendMessage(gameChannel, "You have  " + TIMEOUT_JAIL / 1000 + " seconds to comply");
+
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+
+			}
+		}, TIMEOUT_JAIL);
+
+	}
+
+	private void calculateJail() {
+		bot.sendMessage(mafiaChannel, "Times up mobsters!");
+		stage = Stage.mafiahit;
+
+		ArrayList<String> uniqueElements = new ArrayList<String>();
+		ArrayList<Integer> counts = new ArrayList<Integer>();
+
+		// traverse the array, collect the elements/counts
+		for (String element : hitMap.values()) {
+			int index = uniqueElements.indexOf(element);
+			// element has been seen:
+			if (index != -1) {
+				// increment its count
+				int newCount = counts.get(index) + 1;
+				counts.set(index, newCount);
+			} else {
+				// add it to uniqueElements, set its count to 1
+				uniqueElements.add(element);
+				counts.add(1);
+			}
+		}
+
+		// find maximum count, get the corresponding element
+		int maxCount = 0;
+		int index = -1;
+
+		for (int i = 0; i < counts.size(); i++) {
+			if (maxCount < counts.get(i)) {
+				maxCount = counts.get(i);
+				index = i;
+			}
+		}
+		String target = uniqueElements.get(index);
+
+		hitMap = new HashMap<String, String>();
+
+		bot.sendMessage(gameChannel, "The city people think " + target
+				+ " is a mobster so they locked him up");
+
+		bot.deVoice(gameChannel, target);
+		bot.deVoice(mafiaChannel, target);
+		players.remove(target);
+		mafia.remove(target);
+
+		bot.sendMessage(gameChannel, "Try and work out who the mafia are!");
+		String users = "";
+		for (String player : players) {
+			users += player + " ";
+		}
+		bot.sendMessage(mafiaChannel, "The following people are still alive:" + users);
+		bot.sendMessage(gameChannel, "Once you have made up your choice use " + COMMAND_KILL
+				+ " <target> to lock up a mobster");
+		bot.sendMessage(gameChannel, "You have  " + TIMEOUT_JAIL / 1000 + " seconds to comply");
+
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+
+			}
+		}, TIMEOUT_JAIL);
+
 	}
 
 	@Override
-	public void onJoin(String channel, String sender, String login,
-			String hostname) {
+	public void onJoin(String channel, String sender, String login, String hostname) {
 		if (channel.equals(mafiaChannel)) {
 			if (mafia.contains(sender)) {
 				bot.voice(mafiaChannel, sender);
@@ -227,13 +358,15 @@ public class Mafia extends DefaultPlugin {
 		}
 	}
 
-	public void doHit() {
-		String hitTarget = "";
-		bot.sendMessage(gameChannel,
-				"During the night there was a mafia hit and " + hitTarget
-						+ " died.");
+	@Override
+	public void onAdminCommand(String command, String channel, String sender, String login,
+			String hostname, String message) {
+		bot.sendInvite(sender, gameChannel);
+		bot.sendInvite(sender, mafiaChannel);
+		
 	}
-
+	
+	
 	@Override
 	public String getHelp() {
 		return null;
