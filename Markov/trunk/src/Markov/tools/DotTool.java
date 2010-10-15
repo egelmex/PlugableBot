5533f21ee0165a0c4d58db2c66dbaa9787b89660
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-10-15 15:11:57 +0000

    Adding tool to dump DB as graph

---
+++ Markov/trunk/src/Markov/tools/DotTool.java
@@ -0,0 +1,28 @@
+package Markov.tools;
+
+import com.db4o.ObjectSet;
+
+import Markov.MarkovDatabase;
+import Markov.MarkovLink;
+import Markov.MarkovNode;
+
+public class DotTool {
+	public static void main(String[] args) {
+		MarkovDatabase md = new MarkovDatabase();
+		ObjectSet<MarkovNode> nodes = md.getWords();
+		ObjectSet<MarkovLink> links = md.getLinks();
+		
+		
+		System.out.println("graph brain {");
+		
+		for (MarkovNode node : nodes) {
+			System.out.println(node.getWord() + "[label=\"" + node.getWord() + "\"];");
+		}
+		
+		for (MarkovLink link : links) {
+			System.out.println(link.from + "->" + link.to + ";");
+		}
+			
+		System.out.println("}");
+	}
+}
