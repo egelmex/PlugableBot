e6589a730297070ee3e758d55e436a0990046bc2
mex <mex@fb7b8b94-8d12-4871-9f7a-e55f0cc7a02b>
2010-10-19 21:10:45 +0000

    changing to new command system

--- Random/trunk/src/Random/Random.java
+++ Random/trunk/src/Random/Random.java
@@ -17,84 +17,86 @@
  */
 package Random;
 
-
 import com.PluggableBot.PluggableBot;
 import com.PluggableBot.plugin.DefaultPlugin;
 
 /**
  * RNG plugin for Plugable Bot
+ * 
  * @author Murmew
  * @author Mex
  */
-public class Random extends DefaultPlugin
-{
+public class Random extends DefaultPlugin {
 	public PluggableBot bot;
-	
-    private MersenneTwister mersenneRandom;
-    private java.util.Random random;
-	
-	public Random()
-	{
-        mersenneRandom = new MersenneTwister();
-        random = new java.util.Random(new java.util.Date().getTime());
+
+	private MersenneTwister mersenneRandom;
+	private java.util.Random random;
+
+	private static final String COMMAND = "!random";
+
+	@Override
+	public void load() {
+		bot.addCommand(COMMAND, this);
 	}
 
-    private boolean isNumber(String number)
-    {
-        try {
-            Integer.valueOf(number);
-            return true;
-        }
-        catch (Exception e) { }
-        return false;
-    }
-    
-    private int getRand(int min, int max)
-    {
-        int ret = 0;
-        
-        int tries = 0;
-        do {
-            ret = random.nextInt(max);
-            tries++;
-        } while ((tries < 5000) && (ret < min)); // muh. D:
-        
-        return ret;
-    }
-	
-    @Override
-	public void onMessage(String channel, String sender, String login,
+	public Random() {
+		mersenneRandom = new MersenneTwister();
+		random = new java.util.Random(new java.util.Date().getTime());
+	}
+
+	private boolean isNumber(String number) {
+		try {
+			Integer.valueOf(number);
+			return true;
+		} catch (Exception e) {
+		}
+		return false;
+	}
+
+	private int getRand(int min, int max) {
+		int ret = 0;
+
+		int tries = 0;
+		do {
+			ret = random.nextInt(max);
+			tries++;
+		} while ((tries < 5000) && (ret < min)); // muh. D:
+
+		return ret;
+	}
+
+	@Override
+	public void onCommand(String command, String channel, String sender, String login,
 			String hostname, String message) {
-		if (message.toLowerCase().startsWith("!random")) {
-            String[] parts = message.split(" "); // sorry mex, i dont know an easier way :<
-			
+		if (command.equals(COMMAND)) {
+			String[] parts = message.split(" "); // sorry mex, i dont know an
+													// easier way :<
+
 			int random = 0;
-			if (parts.length == 2) {
-				if (isNumber(parts[1])) {
-					random = getRand(0, Integer.parseInt(parts[1]));
+			if (parts.length == 1) {
+				if (isNumber(parts[0])) {
+					random = getRand(0, Integer.parseInt(parts[0]));
 				}
-			}
-			else if (parts.length == 3) {
-				if (isNumber(parts[1]) && isNumber(parts[2])) {
-					int a = Integer.parseInt(parts[1]);
-					int b = Integer.parseInt(parts[2]);
+			} else if (parts.length == 2) {
+				if (isNumber(parts[0]) && isNumber(parts[1])) {
+					int a = Integer.parseInt(parts[0]);
+					int b = Integer.parseInt(parts[1]);
 					if (a < b) {
 						random = getRand(a, b);
-					}
-					else {
+					} else {
 						bot.Message(channel, sender + ": Nu.");
 						return;
 					}
 				}
-			}
-			else {
+			} else {
 				random = mersenneRandom.extractNumber();
 			}
 			bot.Message(channel, sender + ": Your random number is: " + random);
-        }
+		}
+
 	}
 
-    @Override
+	@Override
 	public String getHelp() {
 		return "Type \"!random optional:min optional:max\" to get a random number!";
 	}
